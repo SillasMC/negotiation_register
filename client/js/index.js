3b08ceda5cc91e263a29ffc465cd33f78c4ad055
Sillas Camara <sillasmc@gmail.com>
2017-10-29 15:35:23 -0300

    Negotiation Controller

--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,1 @@
-let n1 = new Negociacao(new Date(), 10, 50.0);
-
-n1._quantidade = 1000;
-
-console.log(n1.quantidade);
-console.log(n1.data);
-console.log(n1.valor);
-console.log(n1.volume);
+let negociacaoController = new NegociacaoController();
