78ab16fa61d8e6aaafd945168302806c9967e7c9
Sillas Camara <sillasmc@gmail.com>
2017-11-19 16:55:33 -0300

    Order Negotiation List

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,9 +8,11 @@ class NegociacaoController {
 		this._inputQuantidade	= $('#quantidade');
 		this._inputValor		= $('#valor');
 
-		this._listaNegociacoes	= new Bind(new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia');
+		this._listaNegociacoes	= new Bind(new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
 
 		this._mensagem			= new Bind(new Mensagem(), new MensagemView($('#mensagemView')), 'texto');
+
+		this._ordemAtual		= '';
 
 	}
 
@@ -55,5 +57,14 @@ class NegociacaoController {
 			this._mensagem.texto = 'Negociações importadas com sucesso';
 		})
 		.catch(erro => this._mensagem.texto = erro);
+	}
+
+	ordena(coluna) {
+		if (this._ordemAtual == coluna) {
+			this._listaNegociacoes.inverteOrdem();
+		} else {
+			this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+		}
+		this._ordemAtual = coluna;
 	}
 }
